cf7bf73f392af4b7c69c8b381e2b335b6a875164
Asmaa-A7med <asmaaahmed02758@gmail.com>
2025-03-09 02:13:43 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from './nav-bar/nav-bar.component'; 
+import {HomeComponent} from './home/home.component'; 
+import {AboutComponent} from './about/about.component';
+import {SkillsComponent} from './skills/skills.component';
+//import {ProjectsComponent} from './projects/projects.component';
 
+import {ProjectComponent} from './project/project.component';
+import {FooterComponent} from './footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NavBarComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+   // ProjectsComponent,
+    ProjectComponent,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'lab1';
+  title = 'My Protofilio';
 }
---
+++ src/app/footer/README.md
@@ -0,0 +1,21 @@
+# 🖥️ Angular Portfolio Project  
+
+## 📌 Overview  
+This is a **simple portfolio website** built using **Angular** as part of an ITI training task.  
+The project consists of multiple sections showcasing personal details, skills, and projects.  
+
+## 📁 Features  
+- **🏠 Home Page:** Introduction and welcome section.  
+- **👤 About Page:** Personal background and information.  
+- **🛠️ Skills Section:** Displays technical skills.  
+- **📂 Projects Section:** Showcases previous work.  
+
+## ⚙️ Technologies Used  
+- Angular  
+- TypeScript  
+- HTML & CSS  & Bootstrap
+
+## 🚀 How to Run the Project  
+1. Clone the repository:  
+   ```bash
+   git clone <repo-link>
